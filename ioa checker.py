9fdfdee01ae8583f62b97384103b733cca117dc3
nafanh <topkekoutof10@gmail.com>
2022-07-29 03:27:39 -0400

    fixed labeling errors

--- ioa checker.py
+++ ioa checker.py
@@ -21,6 +21,7 @@ class Window(QMainWindow, Ui_MainWindow):
         self.ui.pushButton_2.clicked.connect(self.csv_two)
         self.ui.pushButton_3.clicked.connect(self.calculate)
         self.ui.comboBox.currentTextChanged.connect(self.text_changed)
+
     def csv_one(self):
         try:
             file_name, _ = QFileDialog.getOpenFileName(
@@ -176,11 +177,13 @@ class Window(QMainWindow, Ui_MainWindow):
             total_time = (end_interval - start_interval + 1) * 10
             # print(df1_unique_labels)
             # print(df2_unique_labels)
-            replace_list = ["bt/peers","instructor/screen/on task"]
+            replace_list = ["bt/peers", "instructor/screen/on task"]
             if len(df1_unique_labels) > 1:
-                df1['Label'].replace(df1_unique_labels, replace_list, inplace=True)
-            if len(df1_unique_labels) > 1:
-                df2['Label'].replace(df2_unique_labels, replace_list, inplace=True)
+                df1['Label'].replace(
+                    df1_unique_labels, replace_list, inplace=True)
+            if len(df2_unique_labels) > 1:
+                df2['Label'].replace(
+                    df2_unique_labels, replace_list, inplace=True)
             # print(df1)
             # print(df2)
             df1_fixed_labels = sorted(list(df1['Label'].unique()))
@@ -190,29 +193,44 @@ class Window(QMainWindow, Ui_MainWindow):
             for label in df1_fixed_labels:
                 df_label = df1[df1['Label'] == label].copy()
                 # print(df_label)
-                df_label['time held'] = df_label['Time Released'] - df_label['Time Pressed'] 
-                time_held = round(df_label['time held'].sum(),4)
+                df_label['time held'] = df_label['Time Released'] - \
+                    df_label['Time Pressed']
+                time_held = round(df_label['time held'].sum(), 4)
                 times_1[label] = time_held
-            
+
             for label in df2_fixed_labels:
                 df_label = df2[df2['Label'] == label].copy()
                 # print(df_label)
-                df_label['time held'] = df_label['Time Released'] - df_label['Time Pressed'] 
-                time_held = round(df_label['time held'].sum(),4)
+                df_label['time held'] = df_label['Time Released'] - \
+                    df_label['Time Pressed']
+                time_held = round(df_label['time held'].sum(), 4)
                 times_2[label] = time_held
-            
-            times_1['off target'] = round(total_time - sum(times_1.values()),4)
-            times_2['off target'] = round(total_time - sum(times_2.values()),4)
+
+            times_1['off target'] = round(
+                total_time - sum(times_1.values()), 4)
+            times_2['off target'] = round(
+                total_time - sum(times_2.values()), 4)
 
             ioa_scores_eng = {}
             for key in times_1.keys():
-                if times_1[key] > times_2[key]:
-                    ioa_scores_eng[key] = round(times_2[key] / times_1[key],4)
+                if key in times_2.keys():
+                    if times_1[key] > times_2[key]:
+                        ioa_scores_eng[key] = round(
+                            times_2[key] / times_1[key], 4)
+                    else:
+                        ioa_scores_eng[key] = round(
+                            times_1[key] / times_2[key], 4)
                 else:
-                    ioa_scores_eng[key] = round(times_1[key] / times_2[key],4)
+                    ioa_scores_eng[key] = 0
+            if len(times_1.keys()) < len(times_2.keys()):
+                key_diff = set(list(times_1.keys())) ^ set(
+                    list(times_2.keys()))
+                for label in key_diff:
+                    ioa_scores_eng[label] = 0
+
             text_edit_str = ""
-            for k,v in ioa_scores_eng.items():
-                ioa_value = k + ': ' + str(v) +'\n'
+            for k, v in ioa_scores_eng.items():
+                ioa_value = k + ': ' + str(v) + '\n'
                 text_edit_str += ioa_value
                 if v < float(self.ui.lineEdit_4.text()):
                     self.ui.textEdit.setText(
@@ -220,13 +238,12 @@ class Window(QMainWindow, Ui_MainWindow):
             self.ui.textEdit_2.setText(text_edit_str)
             if self.ui.textEdit.toPlainText() == "":
                 self.ui.textEdit.setText("N/A")
-        
+
                 #print(k, v,sep=': ')
             # print(times_1)
             # print(times_2)
             # print(ioa_scores_eng)
 
-            
         # except:
         #     error_dialog = QErrorMessage()
         #     error_dialog.showMessage('Check inputs, something went wrong')
